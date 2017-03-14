93b96171779ae9c3e03bf09aba197e44071c5098
zeroclan8 <zeroclan8@gmail.com>
2017-03-14 10:51:35 +0800

    port 3000->8080

--- index.js
+++ index.js
@@ -35,4 +35,4 @@ http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write(JSON.stringify(resultObj));
     response.end();
-}).listen(3000);
+}).listen(8080);
